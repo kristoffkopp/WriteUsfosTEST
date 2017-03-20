227504cd1b2a56164c9fb5970b3682acc484b65b
kristoffkopp <kristoffer.kopperud@gmail.com>
2017-03-20 14:45:33 +0100

    Writing crosssection for composits and homoShells

--- WriteUsfosTEST/UsfosCrossSectionWriter.cs
+++ WriteUsfosTEST/UsfosCrossSectionWriter.cs
@@ -13,5 +13,30 @@ namespace WriteUsfosTEST
 
 			return stringList;
 		}
-	}
+
+        public List<string> writeCrossSectionShellHomo(int CrossSectionId, double thickness)
+        {
+            var stringList = new List<string>();
+            string line;
+            line = "PLTHICK " + (CrossSectionId + 1) + "    " + thickness;
+            stringList.Add(line);
+
+            return stringList;
+        }
+
+        public List<string> writeCrossSectionShellComposit(int CompositSectionId, double z0, int[] materialIDs, double[] thicknessList, double[] thetalOrientationList)
+        {
+            var stringList = new List<string>();
+            string line;
+            line = "PLCOMP " + (CompositSectionId + 1) + "    " + z0;
+            if(materialIDs.Length == thicknessList.Length && materialIDs.Length == thetalOrientationList.Length)
+                for (int i = 0; i < materialIDs.Length; i++)
+                {
+                    line = "   " + materialIDs[i] + "   " + thicknessList[i] + "   " + thetalOrientationList[i];
+                }
+            stringList.Add(line);
+
+            return stringList;
+        }
+    }
 }
